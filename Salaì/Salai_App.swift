82a43281e0f7576a5ceeec4e720334cd1da5e39b
claudiomarciello <claudio.marciello99@gmail.com>
2024-02-19 15:01:42 +0100

    Initial Commit

---
+++ Salaì/Salai_App.swift
@@ -0,0 +1,17 @@
+//
+//  Salai_App.swift
+//  Salaì
+//
+//  Created by Claudio Marciello on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Salai_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
