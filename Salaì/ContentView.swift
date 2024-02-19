21939da4743f1ad99e055df25926ba18366f5acd
claudiomarciello <claudio.marciello99@gmail.com>
2024-02-19 16:24:57 +0100

    Localization enabled, Image selection, multiple images selection

--- Salaì/ContentView.swift
+++ Salaì/ContentView.swift
@@ -4,19 +4,44 @@
 //
 //  Created by Claudio Marciello on 19/02/24.
 //
-
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State private var avatarPhotoItems: [PhotosPickerItem] = []
+    @State private var selectedImages: [Image] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Test 2")
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 10) {
+                    ForEach(selectedImages.indices, id: \.self) { index in
+                        selectedImages[index]
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                            .clipShape(Rectangle())
+                    }
+                }
+            }
+            .padding()
+            
+            PhotosPicker("Select images",
+                         selection: $avatarPhotoItems,
+                         matching: .images)
         }
-        .padding()
+        .onChange(of: avatarPhotoItems) { _ in
+            selectedImages.removeAll()
+            Task {
+                for item in avatarPhotoItems {
+                    if let loadedImage = try? await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
     }
 }
 
