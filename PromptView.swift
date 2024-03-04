ca750f1a0ff1d14648da0f35ca215a5994e149d2
claudiomarciello <claudio.marciello99@gmail.com>
2024-03-04 14:35:39 +0100

    Sketch generation, pencilkit

---
+++ PromptView.swift
@@ -0,0 +1,18 @@
+//
+//  PromptView.swift
+//  Salaì
+//
+//  Created by Claudio Marciello on 03/03/24.
+//
+
+import SwiftUI
+
+struct PromptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PromptView()
+}
